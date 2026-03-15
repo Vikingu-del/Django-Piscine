a048a43c14f2960f6677ce61104d29d89440673a
segfault <erik@42vienna.com>
2026-03-15 12:38:15 +0100

    finished Lib0 module

---
+++ Django1/Lib/ex05/djangotutorial/start_server.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/sh
+
+cd ../../ex04/
+source my_script.sh
+cd ../ex05/djangotutorial
+python manage.py runserver
--- Django1/Lib/ex05/initial_setup.sh
+++
@@ -1,2 +0,0 @@
-source ../ex04/my_script.sh
-django-admin startproject helloworld firstproject
