bda524ac163b53a8bf77a0235c39c91fc26773d3
segfault <erik@42vienna.com>
2026-04-07 16:47:55 +0200

    done with first exercise

---
+++ Django1/Base_Django/ex00/d05/ex00/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from django.template import loader
+
+# Create your views here.
+from django.http import HttpResponse
+
+
+def index(request):
+    template = loader.get_template('ex00/index.html')
+    return HttpResponse(template.render({}, request))
