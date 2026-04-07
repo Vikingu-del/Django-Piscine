6c11eaf93352fdf529275712dcc78a5694234852
segfault <erik@42vienna.com>
2026-04-07 07:36:48 +0200

    adding django tutorial

---
+++ all_about_django/django-polls/django_polls/admin.py
@@ -0,0 +1,34 @@
+from django.contrib import admin
+from .models import Question, Choice
+
+# This tells Django: "Choices should be edited on the same page as the Question"
+# class ChoiceInline(admin.StackedInline):
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 0
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    # fields = ["pub_date", "question_text"]
+    fieldsets = [
+        (None, {"fields": ["question_text"]}),
+        (
+            "Date information",
+            {
+                "fields": ["pub_date"],
+                "classes": ["collapse"] 
+            },
+        ),
+    ]
+    inlines = [ChoiceInline]
+    list_display = ["question_text", "pub_date", "was_published_recently"]
+    list_filter = ["pub_date"]
+    search_fields = ["question_text"]
+    list_per_page = 1
+    date_hierarchy = "pub_date"
+
+
+
+admin.site.register(Question, QuestionAdmin)
+
+# Register your models here.
---
+++ all_about_django/django-polls/django_polls/tests.py
@@ -0,0 +1,170 @@
+import datetime
+from django.test import TestCase
+from django.utils import timezone
+from .models import Question
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
+from selenium.webdriver.common.by import By
+from selenium.webdriver.firefox.webdriver import WebDriver
+from selenium.webdriver.firefox.service import Service
+from selenium.webdriver.firefox.options import Options
+
+from django.urls import reverse
+
+# Create your tests here.
+class QuestionModelTests(TestCase):
+    def test_was_published_recently_with_future_question(self):
+        """
+        was_published_recntly() returns False for questions whose pub_date is in the future
+        """
+        time = timezone.now() + datetime.timedelta(days=30)
+        future_question = Question(pub_date=time)
+        self.assertIs(future_question.was_published_recently(), False)
+
+
+    def test_was_published_recently_with_old_question(self):
+        """
+        was_published_recently() returns False for questions whose pub_date is older than 1 day.
+        """
+        time = timezone.now() - datetime.timedelta(days=1, seconds=1)
+        old_question = Question(pub_date=time)
+        self.assertIs(old_question.was_published_recently(), False)
+
+    
+    def test_was_published_recently_with_recent_question(self):
+        """
+        was_published_recently() returns True for questions whose pub_date is within the last day.
+        """
+        time = timezone.now() - datetime.timedelta(hours=23, minutes=59, seconds=59)
+        recent_question = Question(pub_date=time)
+        self.assertIs(recent_question.was_published_recently(), True)
+
+def create_question(question_text, days):
+    """
+    Create a question with the given `question_tet` and published the given number
+    of `days` offset to now (negative for questions published in the past,
+    positive for questions that have yet to be published)
+    """
+    time = timezone.now() + datetime.timedelta(days=days)
+    return Question.objects.create(question_text=question_text, pub_date=time)
+
+
+class QuestionIndexViewTests(TestCase):
+    def test_no_questions(self):
+        """
+        If no questions exist, an appropriate message is displayed.
+        """
+        response = self.client.get(reverse("polls:index"))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "No polls are available.")
+        self.assertQuerySetEqual(response.context["latest_question_list"], [])
+
+    def test_past_question(self):
+        """
+        Questions with a pub_date in the past are displayed on the index page.
+        """
+        question = create_question(question_text="Past question.", days=-30)
+        response = self.client.get(reverse("polls:index"))
+        self.assertQuerySetEqual(
+            response.context["latest_question_list"],
+            [question],
+        )
+
+    def test_future_question(self):
+        """
+        Questions with a pub_date in the future aren't
+        displayed on the index question 
+        """
+        create_question(question_text="Future question.", days=30)
+        response = self.client.get(reverse("polls:index"))
+        self.assertContains(response, "No polls are available.")
+        self.assertQuerySetEqual(response.context["latest_question_list"], [])
+
+    def test_future_question_and_past_question(self):
+        """
+        Even if both past and future questions exist. only past questions are displayed.
+        """
+        question = create_question(question_text="Past question.", days=-30)
+        create_question(question_text="Future question.", days=30)
+        response = self.client.get(reverse("polls:index"))
+        self.assertQuerySetEqual(
+            response.context["latest_question_list"],
+            [question],
+        )
+
+    def test_two_past_questions(self):
+        """"
+        The questions index page may display mutliple quesitons.
+        """
+        question1 = create_question(question_text="Past question1.", days=-30)
+        question2 = create_question(question_text="Past question2.", days=-5)
+        response = self.client.get(reverse("polls:index"))
+        self.assertQuerySetEqual(
+            response.context["latest_question_list"],
+            [question2, question1],
+        )
+
+
+class QuestionDetailViewTests(TestCase):
+    def test_future_question(self):
+        """
+        The detail view of a question with a pub_date in the future returns a 404 not found
+        """
+        past_question = create_question(question_text="Past Question.", days=-5)
+        url = reverse("polls:detail", args=(past_question.id,))
+        response = self.client.get(url)
+        self.assertContains(response, past_question.question_text)
+
+    
+class PollsBrowserTests(StaticLiveServerTestCase):
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        options = Options()
+        options.binary_location = "/snap/firefox/current/usr/lib/firefox/firefox"
+        # options.add_argument("-headless")
+        cls.selenium = WebDriver(options=options)
+        cls.selenium.implicitly_wait(10)
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.selenium.quit()
+        super().tearDownClass()
+
+    def test_question_appears_in_index(self):
+        """
+        A User opens the browser and should see a published question.
+        """
+        # 1. Create data in the test database
+        question_text = "Is Selenium working?"
+        create_question(
+            question_text=question_text,
+            days=-30
+        )
+
+        # 2. Open the index page in the browser
+        # self.libe_server_url automatically handles the localhost:port
+        self.selenium.get(f"{self.live_server_url}{reverse('polls:index')}")
+
+        # 3. Use Selenium to find the question link
+        question_link = self.selenium.find_element(By.LINK_TEXT, question_text)
+
+        # 4. Assert that the link is actually displayed
+        self.assertTrue(question_link.is_displayed())
+
+    def test_future_question_not_visible_to_user(self):
+        """
+        A User should NOT see a future question on the page.
+        """
+        create_question(
+            question_text="Future Question",
+            days=30
+        )
+
+        self.selenium.get(f"{self.live_server_url}{reverse('polls:index')}")
+
+        # Check if the page contains the text "No polls are available."
+        no_polss_p = self.selenium.find_element(By.TAG_NAME, "p")
+        self.assertEqual(no_polss_p.text, "No polls are available.")
+
+
+
