d0b7d4ccc6c86a26a19b8933793f1ece07fa0b43
segfault <erik@42vienna.com>
2026-05-05 18:07:56 +0200

    finished ex04

---
+++ Django3/Advanced/ex04/app/models.py
@@ -0,0 +1,28 @@
+from django.db import models
+from django.conf import settings
+
+# Create your models here.
+class Article(models.Model):
+    title = models.CharField(max_length=64)
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    created = models.DateTimeField(auto_now_add=True)
+    synopsis = models.CharField(max_length=312)
+    content = models.TextField()
+
+    def __str__(self):
+        return self.title
+    
+    class Meta:
+        ordering = ['-created']
+    
+
+class UserFavouriteArticle(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    article = models.ForeignKey(Article, on_delete=models.CASCADE)
+
+    class Meta:
+        unique_together = ('user', 'article')
+    
+    def __str__(self):
+        return self.article.title
+    
