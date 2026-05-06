bae05b21be551ee1f213eedd8df5e41c82b994ab
segfault <erik@42vienna.com>
2026-05-06 12:38:56 +0200

    finished with ex05

---
+++ Django3/Advanced/ex05/init.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+if [ -d "~/.django_env" ]; then
+    rm -rf ~/.django_env
+fi
+python3 -m venv ~/.django_env
+source ~/.django_env/bin/activate
+pip install --upgrade pip
+pip install -r requirements.txt
+
+sudo apt-get update
+sudo apt-get install gettext
