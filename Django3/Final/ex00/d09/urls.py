01ac0b32d6927890200e2e218a01cdc38346ddcb
segfault <erik@42vienna.com>
2026-09-06 20:34:11 +0200

    fixed the issues with the favico

--- Django3/Final/ex00/d09/urls.py
+++ Django3/Final/ex00/d09/urls.py
@@ -17,9 +17,13 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import RedirectView
+from django.http import HttpResponse
 
 urlpatterns = [
+    path("favicon.ico", lambda request: HttpResponse(status=204)),
     path("admin/", admin.site.urls),
+    path("", RedirectView.as_view(url="/account/", permanent=False)),
     path("account/", include("account.urls")),
 ]
 
